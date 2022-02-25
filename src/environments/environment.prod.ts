460fc7bc8572067e282907957f1ae38ddf02adc5
Christian Sucuzhanay <sukuzhanay@gmail.com>
2022-02-25 06:16:18 +0100

    añado credenciales automaticamente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'christian-v-sucuzhanay',
+    appId: '1:116518653690:web:fe6d9cded07efa632e8343',
+    databaseURL: 'https://christian-v-sucuzhanay-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'christian-v-sucuzhanay.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDQ-AygD60MZAN-FBlZD8BPr49f_iS4JWI',
+    authDomain: 'christian-v-sucuzhanay.firebaseapp.com',
+    messagingSenderId: '116518653690',
+    measurementId: 'G-RQS6QZT8TK',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'christian-v-sucuzhanay',
+    appId: '1:116518653690:web:fe6d9cded07efa632e8343',
+    databaseURL: 'https://christian-v-sucuzhanay-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'christian-v-sucuzhanay.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDQ-AygD60MZAN-FBlZD8BPr49f_iS4JWI',
+    authDomain: 'christian-v-sucuzhanay.firebaseapp.com',
+    messagingSenderId: '116518653690',
+    measurementId: 'G-RQS6QZT8TK',
+  },
   production: false
 };
 
