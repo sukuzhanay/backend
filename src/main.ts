4bd19f02e776009101b57eac6d4315e47d791e6a
Christian Sucuzhanay <sukuzhanay@gmail.com>
2022-02-25 05:45:24 +0100

    Import elementos visuales desde ionic

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 if (environment.production) {
   enableProdMode();
