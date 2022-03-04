b2c3aff3571c108a4e3867a01d708f3150f1cc7f
Christian Vladimir Sucuzhanay <sukuzhanay@gmail.com>
2022-03-04 07:07:42 +0100

    Seteamos el servivio auth y creamos 3 funciones, register, login, logout

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  constructor(private auth: Auth) { }
+  async register({mail, passw}){}
+  async login({mail, passw}){}
+  logout(){}
 }
