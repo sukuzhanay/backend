54e6dbc5d224db610b661d169c639fa56182e515
Christian Vladimir Sucuzhanay <sukuzhanay@gmail.com>
2022-03-05 06:32:58 +0100

    add user profile photo and upload to fire/storage

--- src/app/servicios/perfil.service.ts
+++ src/app/servicios/perfil.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Auth } from '@angular/fire/auth';
+import { doc, docData, Firestore, setDoc } from '@angular/fire/firestore';
+import { getDownloadURL, ref, Storage, uploadString } from '@angular/fire/storage';
+import { Photo } from '@capacitor/camera';
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
+  constructor(
+    private auth: Auth,
+    private firestore:Firestore,
+    private storage:Storage
+  ) { }
 
-  constructor() { }
+  getUserProfile(){
+    const user = this.auth.currentUser;
+    const userDocumentRef = doc(this.firestore, `users/${user.uid}`);
+    return docData(userDocumentRef);
+  }
+
+  async uploadImage(image:Photo){
+    const user = this.auth.currentUser;
+    const path = `uploas/${user.uid}/profile.png`
+    const storageRef = ref(this.storage, path);
+    try {
+      await uploadString(storageRef, image.base64String,'base64' );
+      const imageUrl = getDownloadURL(storageRef);
+      const userDocumentRef = doc(this.firestore, `users/${user.uid}`);
+      setDoc(userDocumentRef, imageUrl);
+      return true;
+    } catch (error) {
+      return null;
+    }
+
+  }
 }
+ 
